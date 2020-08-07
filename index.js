df3e4a5b47cd456056d77db2451ad46977ad73c2
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-08-06 21:44:35 -0400

    Create base route in auth.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
+// Import Routes
+const authRoute = require('./routes/auth')
 
-
-
+// Route Middlewares
+app.use('/api/user', authRoute);
 
 app.listen(3001, () => console.log('Server listening on port 3001'));
