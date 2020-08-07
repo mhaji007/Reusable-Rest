e9afc66a8b1ce1d91f563b240c901a3b6bc1713f
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-08-07 17:22:16 -0400

    Add input validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,22 @@
-const User = require('../models/User')
+const User = require('../models/User');
+
+// VALIDATION
+
+const Joi = require('@hapi/joi');
+
+const schema = Joi.object({
+  name: Joi.string().min(6).required(),
+  email: Joi.string().min(6).required().email(),
+  password: Joi.string().min(6).required()
+});
+
 
 exports.getAuth= async(req, res) => {
+
+  const {error} = schema.validate(req.body);
+
+  if (error) return res.status(400).send(error.details[0].message);
+
   const user = new User({
     name: req.body.name,
     email: req.body.email,
