e7f6da0a25e83e4a72decfc7c0b99f8ff3ff52d3
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-08-07 16:03:30 -0400

    Create user model

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,17 @@
-exports.getAuth=(req, res) => {
-  res.send('Register');
+const User = require('../models/User')
+
+exports.getAuth= async(req, res) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  });
+
+  try {
+    const savedUser = await user.save();
+    res.send(savedUser);
+  }catch(err){
+    res.status(400).send(err)
+  }
 
 };
--- index.js
+++ index.js
@@ -2,17 +2,28 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv')
+
+// Import Routes
+const authRoute = require('./routes/auth');
+
 dotenv.config()
 
 // Connect to DB
-mongoose.connect(process.env.MONGO_URI, () => console.log('connected to DB!'))
+mongoose.connect(process.env.MONGO_URI,
+  {useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  },
+  () => console.log('connected to DB!'));
 
-mongoose.connection.on('error', err => {
+mongoose.connection.on('error',
+err => {
   console.log(`DB connection error: ${err.message}`)
 });
 
-// Import Routes
-const authRoute = require('./routes/auth')
+// Mddleware
+app.use(express.json());
 
 // Route Middlewares
 app.use('/api/user', authRoute);
