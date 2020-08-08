85ca3609c6b0c52b30223b747d3fc3d24b6207bc
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-08-08 11:06:28 -0400

    Refactor validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,21 +1,13 @@
 const User = require('../models/User');
-
-// VALIDATION
-
-const Joi = require('@hapi/joi');
-
-const schema = Joi.object({
-  name: Joi.string().min(6).required(),
-  email: Joi.string().min(6).required().email(),
-  password: Joi.string().min(6).required()
-});
+const {registerValidation} = require('../validation');
 
 
 exports.getAuth= async(req, res) => {
 
-  const {error} = schema.validate(req.body);
+  // Validate data before creating a user
+  const {error} = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0]);
 
-  if (error) return res.status(400).send(error.details[0].message);
 
   const user = new User({
     name: req.body.name,
@@ -31,3 +23,40 @@ exports.getAuth= async(req, res) => {
   }
 
 };
+
+/////////////////////////////////////////////////
+
+
+// const User = require('../models/User');
+
+// // VALIDATION
+
+// const Joi = require('@hapi/joi');
+
+// const schema = Joi.object({
+//   name: Joi.string().min(6).required(),
+//   email: Joi.string().min(6).required().email(),
+//   password: Joi.string().min(6).required()
+// });
+
+
+// exports.getAuth= async(req, res) => {
+
+//   const {error} = schema.validate(req.body);
+
+//   if (error) return res.status(400).send(error.details[0].message);
+
+//   const user = new User({
+//     name: req.body.name,
+//     email: req.body.email,
+//     password: req.body.password
+//   });
+
+//   try {
+//     const savedUser = await user.save();
+//     res.send(savedUser);
+//   }catch(err){
+//     res.status(400).send(err)
+//   }
+
+// };
